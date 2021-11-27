24f55aa61b0c318c945349223f61af80a38747b3
Jamil Corwin <kajdhfglksdjhfglkhj@gmail.com>
2021-11-27 19:36:21 +0300

    first-step

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
-#include "main.h";
+#include "main.h"
+
+int     check_args(int argc, char **argv)
+{
+    return (0);
+}
+
+void    init_state(t_state *state, int argc, char **argv)
+{
+
+}
+
+void    parse_args(t_state *state)
+{
+
+}
+
+void    find_solution(t_state *state)
+{
+
+}
+
+void    print_solution(t_state *state)
+{
+
+}
 
 int main(int argc, char **argv) {
     t_state     state;
 
     check_args(argc, argv);
-    init_program(&state, argc, argv);
+    init_state(&state, argc, argv);
     parse_args(&state);
     find_solution(&state);
     print_solution(&state);
